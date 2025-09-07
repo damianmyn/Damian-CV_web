5db909924680620f027079be750f4d9c70597439
sprawler1414 <damian.mynhardt@gmail.com>
2025-09-07 13:41:07 +0200

    added images and .js back

---
+++ Art/artImages.js
@@ -0,0 +1,12 @@
+const artImages = [
+  "abberation.webp",
+  "armani.webp",
+  "damianm.webp",
+  "displace.webp",
+  "everest.webp",
+  "inthedark.webp",
+  "kings.webp",
+  "momento.webp",
+  "starewaytoheaven.webp",
+  "ubridled.webp",
+];
