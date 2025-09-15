1614b7cac8a7da4b148980479e86c33f9d6ef4a9
sprawler1414 <damian.mynhardt@gmail.com>
2025-09-15 11:53:52 +0200

    fixed pathing

--- Certificate/certificateFiles.js
+++ Certificate/certificateFiles.js
@@ -1,4 +1,5 @@
 const certificateFiles = [
     "CompTIA Security  ce certificate.pdf",
-    "Cyber Security (Short Course) Certificate.pdf"
+    "Cyber Security (Short Course) Certificate.pdf",
+    "Data_ScienceZTM.jpg"
 ];
